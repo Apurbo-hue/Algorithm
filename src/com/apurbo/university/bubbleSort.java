4f1d34d4c01427cc78f864d39b6af4547dddb548
Apurbo-hue <labunna123@gmail.com>
2026-03-25 14:19:19 +0600

    bubble sort added

---
+++ src/com/apurbo/university/bubbleSort.java
@@ -0,0 +1,21 @@
+package com.apurbo.university;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int arr []= {10,21,32,14,26};
+
+        for(int i = 0;i < arr.length-1;i++){
+            for (int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("This is the sorted Array(bubble sort):");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
